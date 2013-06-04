56c0eec26f6c9825a63160f7903c4a9b99264716
Doug Avery <dougunderscorenelson@gmail.com>
2013-06-04 10:36:34 -0600

    Added a checkstyle reporter, addressing issue #11

--- tasks/reporters/JSLintXML/index.js
+++ tasks/reporters/JSLintXML/index.js
@@ -1,57 +1,9 @@
-module.exports = function(grunt) {
-
-  var fs = require('fs');
-
-  var getTpl = function(name) {
-      return fs.readFileSync(__dirname + '/' + name).toString();
-    };
-
-  var tpl = {
-    opening: getTpl('opening.xml_'),
-    ending: getTpl('ending.xml_'),
-    violation: getTpl('violation.xml_')
-  };
-
+module.exports = function(grunt, XMLReporter) {
   var JSLintXMLReporter = function(filenames, options) {
-      this.options = options;
-      if(!options.jsLintXML) {
-        throw new Error('Output filename not provided!');
-      }
-      fs.writeFileSync(options.jsLintXML, "");
-    };
-
-  JSLintXMLReporter.prototype = {
-    escape: function(message) {
-      return message.replace(/</g, '&lt;').replace(/>/g, '&gt;');
-    },
-    write: function(message) {
-      fs.appendFileSync(this.options.jsLintXML, message);
-    },
-
-    complexity: function(filepath, complexFunctions) {
-      var message = grunt.template.process(tpl.violation, {
-        data: {
-          filepath: filepath,
-          escape: this.escape,
-          complexFunctions: complexFunctions
-        }
-      });
-      this.write(message);
-    },
-
-    maintainability: function(filepath, valid, analysis) {
-      // Maintainability is not written at all
-    },
-
-    start: function() {
-      this.write(tpl.opening);
-    },
-
-    finish: function() {
-      this.write(tpl.ending);
-    }
+    this.init(options, 'jsLintXML', __dirname);
   };
 
+  JSLintXMLReporter.prototype = new XMLReporter();
 
   return JSLintXMLReporter;
 };
---
+++ tasks/reporters/XML/index.js
@@ -0,0 +1,63 @@
+module.exports = function(grunt) {
+
+  var XMLReporter = function() {};
+  var fs = require('fs');
+
+  XMLReporter.prototype = {
+    init: function(options, fileKey, dirname) {
+      this.options = options;
+      this.xmlFilename = options[fileKey];
+      if(!this.xmlFilename) {
+        throw new Error('Output filename not provided!');
+      }
+      this.dirname = dirname;
+      this.tpl = this.getTpl();
+      fs.writeFileSync(this.xmlFilename, "");
+    },
+
+    readTpl: function(name) {
+      return fs.readFileSync(this.dirname + '/' + name).toString();
+    },
+
+    getTpl: function() {
+      return {
+        opening: this.readTpl('opening.xml_'),
+        ending: this.readTpl('ending.xml_'),
+        violation: this.readTpl('violation.xml_')
+      };
+    },
+
+    escape: function(message) {
+      return message.replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    },
+
+    write: function(message) {
+      fs.appendFileSync(this.xmlFilename, message);
+    },
+
+    complexity: function(filepath, complexFunctions) {
+      var message = grunt.template.process(this.tpl.violation, {
+        data: {
+          filepath: filepath,
+          escape: this.escape,
+          complexFunctions: complexFunctions
+        }
+      });
+      this.write(message);
+    },
+
+    maintainability: function(filepath, valid, analysis) {
+      // Maintainability is not written at all
+    },
+
+    start: function() {
+      this.write(this.tpl.opening);
+    },
+
+    finish: function() {
+      this.write(this.tpl.ending);
+    }
+  };
+
+  return XMLReporter;
+};
