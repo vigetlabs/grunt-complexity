d69548354698b8a11ae4788f56cfcc16f3098d3a
Stuart Campbell <stuart.campbell@trinitymirror.com>
2015-08-04 11:47:30 +0100

    Allow option to only fail build on errors and allow lower severities to pass

--- tasks/complexity.js
+++ tasks/complexity.js
@@ -18,7 +18,8 @@ module.exports = function(grunt) {
 			cyclomatic: [3, 7, 12],
 			halstead: [8, 13, 20],
 			maintainability: 100,
-			hideComplexFunctions: false
+			hideComplexFunctions: false,
+			allowWarnings: false
 		},
 
 		normalizeOptions: function(options) {
@@ -117,7 +118,11 @@ module.exports = function(grunt) {
 				}, this);
 			}
 
-			grunt.fail.errorcount += complicatedFunctions.length;
+			var errors = complicatedFunctions.filter(function(data){
+				return data.severity === "error";
+			});
+
+			grunt.fail.errorcount += (options.allowWarnings ? errors.length : complicatedFunctions.length);
 
 			reporter.complexity(filepath, complicatedFunctions);
 
